b845b6e5bca489dd93a3937ff29825e3d280e845
j4n9k4 <janukananayakkara@gmail.com>
2025-02-08 12:56:33 +0530

    Re-usable Class with generics

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,13 @@
-class FillInTheBlankQuestion(
+class Question<T>(
     val questionText: String,
-    val answer: String,
+    val answer: T,
     val difficulty: String
-)
 
-class TrueOrFalseQuestion(
-    val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
-    val difficulty: String
 )
 fun main()
 {
 
+    val question1 = Question<String>("What is your name", "Lard", "Easy")
+    val question2 = Question<Boolean>("Is the sky green", false, "Easy")
+    val question3 = Question<Int>("What is 2 + 2", 4, "Easy")
 }
