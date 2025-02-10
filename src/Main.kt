29a79dfe065ed2fef7afe7c9f0afe3da9eda8ccb
j4n9k4 <janukananayakkara@gmail.com>
2025-02-10 13:45:20 +0530

    Singleton Objects

--- src/Main.kt
+++ src/Main.kt
@@ -2,16 +2,22 @@ enum class Difficulty {
     EASY, Medium, Hard
 }
 
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
 
 )
+object StudentProgress{
+    var total:Int = 10
+    var answered:Int = 3
+}
 fun main()
 {
 
     val question1 = Question<String>("What is your name", "Lard", Difficulty.EASY)
     val question2 = Question<Boolean>("Is the sky green", false, Difficulty.EASY)
     val question3 = Question<Int>("What is 2 + 2", 4, Difficulty.EASY)
+
+    println("${StudentProgress.answered} of ${StudentProgress.total} answered")
 }
