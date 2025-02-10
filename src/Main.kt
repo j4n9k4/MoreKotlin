d45bd905cfbf77939bd74dacd62a33799d5e1dde
j4n9k4 <janukananayakkara@gmail.com>
2025-02-10 12:50:26 +0530

    Enum Class

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,17 @@
+enum class Difficulty {
+    EASY, Medium, Hard
+}
+
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 
 )
 fun main()
 {
 
-    val question1 = Question<String>("What is your name", "Lard", "Easy")
-    val question2 = Question<Boolean>("Is the sky green", false, "Easy")
-    val question3 = Question<Int>("What is 2 + 2", 4, "Easy")
+    val question1 = Question<String>("What is your name", "Lard", Difficulty.EASY)
+    val question2 = Question<Boolean>("Is the sky green", false, Difficulty.EASY)
+    val question3 = Question<Int>("What is 2 + 2", 4, Difficulty.EASY)
 }
