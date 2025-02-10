a97f2b3314b72133efda2a9e8f1d39b1e5dd338b
j4n9k4 <janukananayakkara@gmail.com>
2025-02-10 13:48:38 +0530

    Re-Factoring the code as a companion object

--- src/Main.kt
+++ src/Main.kt
@@ -8,16 +8,22 @@ data class Question<T>(
     val difficulty: Difficulty
 
 )
-object StudentProgress{
-    var total:Int = 10
-    var answered:Int = 3
-}
-fun main()
-{
 
+class Quiz()
+{
     val question1 = Question<String>("What is your name", "Lard", Difficulty.EASY)
     val question2 = Question<Boolean>("Is the sky green", false, Difficulty.EASY)
     val question3 = Question<Int>("What is 2 + 2", 4, Difficulty.EASY)
 
-    println("${StudentProgress.answered} of ${StudentProgress.total} answered")
+    companion object StudentProgress{
+        var total:Int = 10
+        var answered:Int = 3
+    }
+}
+fun main()
+{
+
+
+
+    println("${Quiz.StudentProgress.answered} of ${Quiz.StudentProgress.total} answered")
 }
