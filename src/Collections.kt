37834889ea24f1b77748d105da7c9a382c380864
j4n9k4 <janukananayakkara@gmail.com>
2025-02-12 13:27:30 +0530

    Arrays Finished

--- src/Collections.kt
+++ src/Collections.kt
@@ -1,3 +1,12 @@
 fun main() {
 
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Mars","Earth")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+
+    val solarSystem = rockPlanets + gasPlanets
+
+    solarSystem[2] = "Little Earth"
+
+    println(solarSystem[0])
+    println(solarSystem[2])
 }
