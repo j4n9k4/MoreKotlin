5e0d26b20c3cb36645bfd39148d918864028a338
j4n9k4 <janukananayakkara@gmail.com>
2025-02-13 20:02:37 +0530

    Finished list

--- src/List.kt
+++ src/List.kt
@@ -1,6 +1,6 @@
 fun main()
 {
-    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn")
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn")
     println(solarSystem.size)
     println(solarSystem[2])
     println(solarSystem.get(3))
@@ -12,4 +12,16 @@ fun main()
     {
         println(planet)
     }
+
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+
+    println(solarSystem[3])
+    println(solarSystem[7])
+
+    solarSystem.removeAt(7)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 }
